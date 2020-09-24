411f5a9350b32e50a81114a447055c3c41afa975
skotambkar <kotambka@amazon.com>
2020-09-24 00:25:32 -0700

    add customization to disable auto decode gzip for s3. Moves customizations from dynamodb to reuse middleware

--- service/dynamodb/internal/customizations/doc.go
+++ service/dynamodb/internal/customizations/doc.go
@@ -30,17 +30,9 @@ Customization option:
 
 Accept encoding gzip
 
-The Go HTTP client automatically supports accept-encoding and content-encoding
-gzip by default. This default behavior is not desired by the SDK, and prevents
-validating the response body's checksum. To prevent this the SDK must manually
-control usage of content-encoding gzip.
-
-To control content-encoding, the SDK must always set the `Accept-Encoding`
-header to a value. This prevents the HTTP client from using gzip automatically.
-When gzip is enabled on the API client, the SDK's customization will control
-decompressing the gzip data in order to not break the checksum validation. When
-gzip is disabled, the API client will disable gzip, preventing the HTTP
-client's default behavior.
+For customization around accept encoding, dynamodb client uses the middlewares
+defined at service/internal/accept-encoding. Please refer to the documentation for
+`accept-encoding` package for more details.
 
 Customization option:
     EnableAcceptEncodingGzip (Disabled by Default)
